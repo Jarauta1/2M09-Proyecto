c48e14084f6025d10bf0848a1b9cf26cf634ca1c
Diego Jarauta <diegojarauta8@gmail.com>
2020-11-17 20:04:15 +0100

    Carga a dashboard de todos los datos totales

--- datos.js
+++ datos.js
@@ -3,20 +3,82 @@ const router = express.Router();
 const cifrarContrasenia = require("./cifrarContrasenia")
 const bcrypt = require("bcrypt");
 
-
+let actividades = 0
+let calorias = 0
+let desnivel = 0
+let media = 0
+let maxima = 0
+let asics = 0
+let mizuno = 0
+let distancia = 0
+let tiempo = 0
 
 router.post("/", function(req, res) {
     let db = req.app.locals.db
 
     let username = req.body.username
+    let anyo = parseInt(req.body.anyo)
+    let mes = req.body.mes
 
-    db.collection("users").find({ $and: [{ username: username }, { entrenamiento: { $elemMatch: { anyo: 2019 } } }] }).toArray(function(err, datos) {
+    db.collection("users").find({ $and: [{ username: username }] }).toArray(function(err, datos) {
         if (err !== null) {
             console.log(err)
             res.send({ mensaje: "Error:" + err })
         } else {
-            let entrenamiento = datos[0].entrenamiento
-            res.send(entrenamiento)
+            db.collection("users").find({ $and: [{ entrenamiento: { $elemMatch: { anyo: 2019 } } }, { entrenamiento: { $elemMatch: { mes: "junio" } } }] }).toArray(function(err, datos) {
+                if (err !== null) {
+                    console.log(err)
+                    res.send({ mensaje: "Error:" + err })
+                } else {
+                    let entrenamiento = datos[0].entrenamiento
+                    let contador = 0
+                    for (let i = 0; i < entrenamiento.length; i++) {
+                        if (entrenamiento[i].mes == "total") {
+                            contador++
+                            actividades += entrenamiento[i].actividades
+                            calorias += entrenamiento[i].calorias / 1000
+                            desnivel += entrenamiento[i].elevacion / 1000
+                            media += entrenamiento[i].velocidadMedia
+                            maxima += entrenamiento[i].velocidadMaxima
+                            distancia += entrenamiento[i].distancia / 1000
+                            tiempo += entrenamiento[i].tiempo
+                        }
+                        if (entrenamiento[i].zapatillas == "ASICS" && entrenamiento[i].mes !== "total") {
+                            asics += entrenamiento[i].distancia / 1000
+                        }
+                        if (entrenamiento[i].zapatillas == "MIZUNO" && entrenamiento[i].mes !== "total") {
+                            mizuno += entrenamiento[i].distancia / 1000
+                        }
+                    }
+
+                    calorias = calorias.toFixed(2)
+                    desnivel = desnivel.toFixed(2)
+                    media = media / contador
+                    maxima = maxima / contador
+                    media = media.toFixed(2)
+                    maxima = maxima.toFixed(2)
+                    asics = asics.toFixed(2)
+                    mizuno = mizuno.toFixed(2)
+                    distancia = distancia.toFixed(2)
+
+                    let segundos = tiempo % 60
+                    let segundosOperar = tiempo - segundos
+
+                    let minutosCalculo = segundosOperar / 60
+                    let minutos = minutosCalculo % 60
+                    let minutosOperar = minutosCalculo - minutos
+
+                    let horasCalculo = minutosOperar / 60
+                    let horas = horasCalculo % 24
+                    let horasOperar = horasCalculo - horas
+
+                    let diasCalculo = horasOperar / 24
+                    let dias = diasCalculo % 24
+
+
+                    res.send({ actividades: actividades, calorias: calorias, desnivel: desnivel, media: media, maxima: maxima, asics: asics, mizuno: mizuno, distancia: distancia, segundos: segundos, minutos: minutos, horas: horas, dias: dias })
+                }
+            })
         }
     })
 
--- public/JS/dashboard.js
+++ public/JS/dashboard.js
@@ -3,50 +3,73 @@ let mensaje = ""
 let username = localStorage.getItem("usuario")
 console.log(username)
 
+let anyo
+let mes
+
+function editar() {
+    location.href = '../editarPerfil.html';
+}
 
 
 actualizarDatos()
     /* addDatos() */
 
 function actualizarDatos() {
+
+    /* let anyo = document.getElementById("selectAnyo").value
+    let mes = document.getElementById("selectMes").value */
+
     fetch("/datos/", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ username: username }),
+            body: JSON.stringify({ username: username, anyo: anyo, mes: mes }),
         })
         .then(function(res) {
             return res.json();
         })
         .then(function(data) {
             console.log(data)
-            console.log(data.length, data[1].anyo, data[1].mes)
 
-            for (let i = 0; i < data.length; i++) {
-                if (data[i].anyo == 2019 && data[i].mes == "total") {
-                    console.log(data[i].calorias)
-                    document.getElementById("calorias").innerHTML = data[i].calorias
-                    document.getElementById("desnivel").innerHTML = (data[i].elevacion / 1000)
-                    document.getElementById("distancia").innerHTML = `${(data[i].distancia / 1000)} Km`
 
-                    let segundos = (data[i].tiempo % 60)
-                    let segundosOperar = (data[i].tiempo - segundos)
 
-                    let minutosCalculo = segundosOperar / 60
-                    let minutos = minutosCalculo % 60
-                    let minutosOperar = minutosCalculo - minutos
+            document.getElementById("actividades").innerHTML = data.actividades
+            document.getElementById("calorias").innerHTML = data.calorias
+            document.getElementById("desnivel").innerHTML = data.desnivel
+            document.getElementById("media").innerHTML = data.media
+            document.getElementById("maxima").innerHTML = data.maxima
+            document.getElementById("zapatilla1").innerHTML = data.asics
+            document.getElementById("zapatilla2").innerHTML = data.mizuno
+            document.getElementById("tiempo").innerHTML = `${data.dias} días ${data.horas} h:${data.minutos} min`
+            document.getElementById("segundos").innerHTML = `:${data.segundos} seg`
 
-                    let horasCalculo = minutosOperar / 60
-                    let horas = horasCalculo % 60
-                    let horasOperar = horasCalculo - horas
+            /*  console.log(data.length, data[1].anyo, data[1].mes)
 
-                    let diasCalculo = horasOperar / 24
-                    let dias = diasCalculo % 60
+             for (let i = 0; i < data.length; i++) {
+                 if (data[i].anyo == 2019 && data[i].mes == "total") {
+                     console.log(data[i].calorias)
+                     document.getElementById("calorias").innerHTML = data[i].calorias
+                     document.getElementById("desnivel").innerHTML = (data[i].elevacion / 1000)
+                     document.getElementById("distancia").innerHTML = `${(data[i].distancia / 1000)} Km`
 
-                    document.getElementById("tiempo").innerHTML = `${dias} d, ${horas} h, ${minutos} min y ${segundos} seg.`
-                }
-            }
+                     let segundos = (data[i].tiempo % 60)
+                     let segundosOperar = (data[i].tiempo - segundos)
+
+                     let minutosCalculo = segundosOperar / 60
+                     let minutos = minutosCalculo % 60
+                     let minutosOperar = minutosCalculo - minutos
+
+                     let horasCalculo = minutosOperar / 60
+                     let horas = horasCalculo % 60
+                     let horasOperar = horasCalculo - horas
+
+                     let diasCalculo = horasOperar / 24
+                     let dias = diasCalculo % 60
+
+                     document.getElementById("tiempo").innerHTML = `${dias} d, ${horas} h, ${minutos} min y ${segundos} seg.`
+                 }
+             } */
 
         });
 }
@@ -72,3 +95,69 @@ function addDatos() {
 }
 
 /* document.getElementById("prueba").innerHTML = */
+
+var options = {
+    series: [{
+        name: "2020",
+        data: [0, 0, 0, 0, 94.16, 96.56, 32.14, 32.17, 40.35, 64.44, 8]
+    }],
+    chart: {
+        height: 200,
+        type: 'line',
+        dropShadow: {
+            enabled: true,
+            color: '#000',
+            top: 18,
+            left: 7,
+            blur: 10,
+            opacity: 0.2
+        },
+        toolbar: {
+            show: false
+        }
+    },
+    colors: ['#77B6EA', '#545454'],
+    dataLabels: {
+        enabled: true,
+    },
+    stroke: {
+        curve: 'smooth'
+    },
+    title: {
+        text: 'Distancia',
+        align: 'left'
+    },
+    grid: {
+        borderColor: '#e7e7e7',
+        row: {
+            colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+            opacity: 0.5
+        },
+    },
+    markers: {
+        size: 1
+    },
+    xaxis: {
+        categories: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', "Ago", "Sep", "Oct", "Nov"],
+        title: {
+            text: 'Mes'
+        }
+    },
+    yaxis: {
+        title: {
+            text: 'Km'
+        },
+        min: 5,
+        max: 100
+    },
+    legend: {
+        position: 'bottom',
+        horizontalAlign: 'right',
+        floating: true,
+        offsetY: -25,
+        offsetX: -5
+    }
+};
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
