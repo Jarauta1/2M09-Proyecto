21bab58632f08925c38d2a2958dedb4e772f93dd
Diego Jarauta <diegojarauta8@gmail.com>
2020-11-20 10:56:10 +0100

    solicitud amistad terminada

--- public/JS/listaSolicitud.js
+++ public/JS/listaSolicitud.js
@@ -73,33 +73,48 @@ function amistad(indice) {
 let propio
 
 function aceptar(indice) {
-    for (i = 0; i < data.length; i++) {
-        if (data[i].nombre === usuario) {
-            propio = i
-        }
-    }
-    data[indice2].amistad.push({ nombre: data[propio].nombre, apellido: data[propio].apellido })
-    data[indice2].solicitud = "no"
-    data[indice2].peticion = "no"
-    data[propio].amistad.push({ nombre: data[indice2].nombre, apellido: data[indice2].apellido })
-    data[propio].solicitud = "no"
-    data[propio].peticion = "no"
 
+    let solicitante = usuario
+    let solicitado = localStorage.getItem("usuario")
 
-
+    fetch("/solicitudesAmistad/aceptar", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ solicitado: solicitado, solicitante: solicitante }),
+        })
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(data) {
+            console.log(data)
+            localStorage.setItem("solicitud", "no")
+            localStorage.setItem("solicitante", "nadie")
+            window.alert(data.mensaje)
+        });
 }
 
 function rechazar() {
 
-    data[indice2].solicitud = "no"
-    data[indice2].peticion = "no"
-    for (i = 0; i < data.length; i++) {
-        if (data[i].nombre === usuario) {
-            let propio = i
-        }
-    }
 
-    data[propio].solicitud = "no"
-    data[propio].peticion = "no"
+    let solicitante = usuario
+    let solicitado = localStorage.getItem("usuario")
 
+    fetch("/solicitudesAmistad/rechazar", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ solicitado: solicitado, solicitante: solicitante }),
+        })
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(data) {
+            console.log(data)
+            localStorage.setItem("solicitud", "no")
+            localStorage.setItem("solicitante", "nadie")
+            window.alert(data.mensaje)
+        });
 }
